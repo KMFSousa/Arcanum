dd521d90153a01500642051260cad72af55b4b8a
Jackson Barr <jbarr@uwaterloo.ca>
2020-06-03 16:36:42 -0400

    Monster class created

---
+++ app/src/main/java/org/o7planning/android2dgame/Monster.java
@@ -0,0 +1,9 @@
+package org.o7planning.android2dgame;
+
+import android.graphics.Bitmap;
+
+public class Monster extends Character {
+    public Monster(GameSurface gameSurface, Bitmap image, int x, int y) {
+        super(gameSurface, image, x, y);
+    }
+}
